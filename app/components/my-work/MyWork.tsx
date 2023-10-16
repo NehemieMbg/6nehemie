870bb91d0c27b3314ef298795f5ba5d0ffc6df28
NehemieMbg <nehemie.mbg@gmail.com>
2023-10-16 20:55:13 +0200

    project

--- app/components/my-work/MyWork.tsx
+++ app/components/my-work/MyWork.tsx
@@ -1,31 +1,64 @@
-import Image from 'next/image';
-import { recentProjects } from '../../constants';
-import { ArrowUpRightIcon } from '@heroicons/react/24/outline';
-import { ProjectCard } from '..';
+'use client';
 
-const project = recentProjects[0];
+import { recentProjects } from '../../constants';
+import { ProjectCard } from '..';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 
 const MyWork = () => {
+  const [showMore, setShowMore] = useState(false);
+  const [projectCount, setProjectCount] = useState(2); // 0 based index
+
+  const handleShowMore = () => {
+    setShowMore(true);
+    setProjectCount(recentProjects.length - 1);
+  };
+
+  const handleShowLess = () => {
+    setShowMore(false);
+    setProjectCount(2);
+  };
   return (
     <section className="p-10 font-montserrat">
       <div>
         <h2 className="text-2xl uppercase mb-2 font-semibold">
           Recent Projects
         </h2>
-        <p className="mb-10">Showcasing some of my latest work.</p>
+        <div className="flex items-start justify-between">
+          <p className="mb-10">Showcasing some of my latest work.</p>
+          {!showMore ? (
+            <button
+              onClick={handleShowMore}
+              className="text-center flex items-center gap-2"
+            >
+              <span>See more</span> <ChevronDownIcon className="h-4" />
+            </button>
+          ) : (
+            <button
+              onClick={handleShowLess}
+              className="text-center flex items-center gap-2"
+            >
+              <span>See less</span> <ChevronUpIcon className="h-4" />
+            </button>
+          )}
+        </div>
       </div>
 
-      <div className="grid grid-cols-3 gap-4">
-        {/* PROJECT CARD */}
-        {recentProjects.map((project) => (
-          <ProjectCard
-            key={project.label}
-            label={project.label}
-            description={project.description}
-            images={project.images}
-            link={project.link}
-          />
-        ))}
+      {/* PROJECT CARD */}
+      <div className="grid grid-cols-3 gap-4 mb-8">
+        {recentProjects.map((project, index) => {
+          if (index > projectCount) return null;
+
+          return (
+            <ProjectCard
+              key={project.label}
+              label={project.label}
+              description={project.description}
+              images={project.images}
+              link={project.link}
+            />
+          );
+        })}
       </div>
     </section>
   );
